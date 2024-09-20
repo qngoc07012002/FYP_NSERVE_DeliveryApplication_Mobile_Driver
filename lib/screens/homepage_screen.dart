36f8ad07a5b0a88675e8e322a2fb59062997039a
Ngoc <quangngoc.07012002@gmail.com>
2024-09-20 22:11:01 +0700

    Home Page Screen

--- lib/screens/homepage_screen.dart
+++ lib/screens/homepage_screen.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:deliveryapplication_mobile_driver/screens/message_screen.dart';
 import 'package:deliveryapplication_mobile_driver/screens/order_screen.dart';
 import 'package:deliveryapplication_mobile_driver/screens/profile_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:deliveryapplication_mobile_driver/screens/services/getlocation.dart';
 
 class DriverHomePage extends StatefulWidget {
   const DriverHomePage({super.key});
@@ -13,53 +14,50 @@ class DriverHomePage extends StatefulWidget {
 
 class _DriverHomePageState extends State<DriverHomePage> {
   int _selectedIndex = 0;
+  bool isOnline = false;
+  bool _isLoading = true; // Thêm biến trạng thái loading
+  MapboxMap? mapboxMap;
+  LocationService _locationService = LocationService();
+  late double _latitude;
+  late double _longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      Map<String, double>? location = await _locationService.getCurrentLocation();
+      if (location != null) {
+        setState(() {
+          _latitude = location['latitude']!;
+          _longitude = location['longitude']!;
+          _isLoading = false;
+        });
+      }
+    } catch (e) {
+      print(e);
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  _onMapCreated(MapboxMap mapboxMap) {
+    this.mapboxMap = mapboxMap;
+  }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      // Here you can handle navigation logic based on the selected index
       print('Selected index: $_selectedIndex');
     });
   }
 
-  bool isOnline = false;
-
-  // Sample data
-  final List<ChartData> dailyData = [
-    ChartData('Mon', 30),
-    ChartData('Tue', 40),
-    ChartData('Wed', 25),
-    ChartData('Thu', 45),
-    ChartData('Fri', 50),
-    ChartData('Sat', 35),
-    ChartData('Sun', 60),
-  ];
-
-  final List<ChartData> monthlyData = [
-    ChartData('Jan', 200),
-    ChartData('Feb', 220),
-    ChartData('Mar', 250),
-    ChartData('Apr', 300),
-    ChartData('May', 280),
-    ChartData('Jun', 350),
-    ChartData('Jul', 400),
-    ChartData('Aug', 370),
-    ChartData('Sep', 420),
-    ChartData('Oct', 450),
-    ChartData('Nov', 460),
-    ChartData('Dec', 490),
-  ];
-
-  final List<ChartData> yearlyData = [
-    ChartData('2022', 5000),
-    ChartData('2023', 6000),
-    ChartData('2024', 7000),
-  ];
-
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -88,7 +86,7 @@ class _DriverHomePageState extends State<DriverHomePage> {
       body: IndexedStack(
         index: _selectedIndex,
         children: [
-          _buildHomePage(), // The home page content
+          _buildHomePage(),
           OrderPage(),
           MessagePage(),
           ProfilePage(),
@@ -98,18 +96,37 @@ class _DriverHomePageState extends State<DriverHomePage> {
   }
 
   Widget _buildHomePage() {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Header with avatar, name, and status toggle button
-          Container(
-            padding: const EdgeInsets.fromLTRB(16, 60, 16, 30),
+    return Stack(
+      children: [
+        // Map section
+        Positioned.fill(
+          child: _isLoading
+              ? Center(
+            child: CircularProgressIndicator(),
+          )
+              : MapWidget(
+            key: const ValueKey("mapWidget"),
+            resourceOptions: ResourceOptions(
+                accessToken:
+                "pk.eyJ1IjoicW5nb2MwNzAxMjAwMiIsImEiOiJjbTE0MDkwbWkxZ3IwMnZxMjB2ejBkaGZnIn0.cuJH5sW_W10ZWlQpIb67dw"),
+            cameraOptions: CameraOptions(
+                center: Point(coordinates: Position(_longitude, _latitude)).toJson(),
+                zoom: 17),
+            styleUri: MapboxStyles.DARK,
+            textureView: true,
+            onMapCreated: _onMapCreated,
+          ),
+        ),
+        // Header with avatar, name, and status toggle button
+        Positioned(
+          top: 40, // Adjust as needed
+          left: 16,
+          right: 16,
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
             decoration: BoxDecoration(
-              color: const Color(0xFF39c5c8), // Màu xanh chủ đạo
-              borderRadius: const BorderRadius.vertical(
-                bottom: Radius.circular(20.0), // Bo tròn góc dưới
-              ),
+              color: Colors.white.withOpacity(0.6), // Semi-transparent background for better readability
+              borderRadius: BorderRadius.circular(10),
             ),
             child: Row(
               children: [
@@ -125,11 +142,11 @@ class _DriverHomePageState extends State<DriverHomePage> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        'John Doe',
+                        'Béo Shipper',
                         style: const TextStyle(
-                          fontSize: 20,
+                          fontSize: 18,
                           fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                          color: Colors.black,
                         ),
                       ),
                       const SizedBox(height: 4.0),
@@ -147,7 +164,7 @@ class _DriverHomePageState extends State<DriverHomePage> {
                           Text(
                             isOnline ? 'Online' : 'Offline',
                             style: TextStyle(
-                              fontSize: 16,
+                              fontSize: 14,
                               fontWeight: FontWeight.bold,
                               color: isOnline ? Colors.green : Colors.red,
                             ),
@@ -165,218 +182,17 @@ class _DriverHomePageState extends State<DriverHomePage> {
                   },
                   child: Text(isOnline ? 'Go Offline' : 'Go Online'),
                   style: ElevatedButton.styleFrom(
-                    foregroundColor: const Color(0xFF39c5c8),
-                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.white, backgroundColor: const Color(0xFF39c5c8),
                   ),
                 ),
               ],
             ),
           ),
-
-          // Revenue and Orders section
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Card(
-                    color: Colors.white,
-                    elevation: 4,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Today\'s Revenue',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black.withOpacity(0.8),
-                            ),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            '\$150',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16.0),
-                Expanded(
-                  child: Card(
-                    color: Colors.white,
-                    elevation: 4,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Today\'s Orders',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black.withOpacity(0.8),
-                            ),
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            '20 Orders',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-
-          // Revenue section with charts
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Daily Revenue Chart
-                Card(
-                  elevation: 4,
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 200,
-                          child: SfCartesianChart(
-                            primaryXAxis: CategoryAxis(),
-                            primaryYAxis: NumericAxis(),
-                            series: <CartesianSeries>[
-                              ColumnSeries<ChartData, String>(
-                                dataSource: dailyData,
-                                xValueMapper: (ChartData data, _) => data.x,
-                                yValueMapper: (ChartData data, _) => data.y,
-                                color: Colors.blue,
-                                dataLabelSettings: DataLabelSettings(isVisible: true),
-                              ),
-                            ],
-                            title: ChartTitle(
-                              text: 'Daily Revenue: \$400',
-                              alignment: ChartAlignment.center,
-                              textStyle: TextStyle(color: Colors.black.withOpacity(0.8)),
-                            ),
-                            margin: EdgeInsets.symmetric(vertical: 16),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-
-                // Monthly Revenue Chart
-                Card(
-                  elevation: 4,
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 300,
-                          child: SfCartesianChart(
-                            primaryXAxis: CategoryAxis(),
-                            primaryYAxis: NumericAxis(),
-                            series: <CartesianSeries>[
-                              LineSeries<ChartData, String>(
-                                dataSource: monthlyData,
-                                xValueMapper: (ChartData data, _) => data.x,
-                                yValueMapper: (ChartData data, _) => data.y,
-                                color: Colors.green,
-                                dataLabelSettings: DataLabelSettings(isVisible: true),
-                              ),
-                            ],
-                            title: ChartTitle(
-                              text: 'Monthly Revenue: \$2500',
-                              alignment: ChartAlignment.center,
-                              textStyle: TextStyle(color: Colors.black.withOpacity(0.8)),
-                            ),
-                            margin: EdgeInsets.symmetric(vertical: 16),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-
-                // Yearly Revenue Chart
-                Card(
-                  elevation: 4,
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 300,
-                          child: SfCartesianChart(
-                            primaryXAxis: CategoryAxis(),
-                            primaryYAxis: NumericAxis(),
-                            series: <CartesianSeries>[
-                              ColumnSeries<ChartData, String>(
-                                dataSource: yearlyData,
-                                xValueMapper: (ChartData data, _) => data.x,
-                                yValueMapper: (ChartData data, _) => data.y,
-                                color: Colors.red,
-                                dataLabelSettings: DataLabelSettings(isVisible: true),
-                              ),
-                            ],
-                            title: ChartTitle(
-                              text: 'Yearly Revenue: \$18000',
-                              alignment: ChartAlignment.center,
-                              textStyle: TextStyle(color: Colors.black.withOpacity(0.8)),
-                            ),
-                            margin: EdgeInsets.symmetric(vertical: 16),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
-
-class ChartData {
-  ChartData(this.x, this.y);
-  final String x;
-  final double y;
-}
-
-
 
 void main() {
   runApp(const MaterialApp(
