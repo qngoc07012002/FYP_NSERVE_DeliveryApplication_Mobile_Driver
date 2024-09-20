16fb1655d5365e5acfb3f9b38c59fc76842a2641
Ngoc <quangngoc.07012002@gmail.com>
2024-09-20 22:10:15 +0700

    Update Permission Location

--- android/app/src/main/kotlin/com/example/deliveryapplication_mobile_driver/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/deliveryapplication_mobile_driver/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.deliveryapplication_mobile_driver
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
