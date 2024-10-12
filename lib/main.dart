4c47eb340f11ce9f0b86dd80d39aad93cc7c8662
Ngoc <quangngoc.07012002@gmail.com>
2024-10-12 18:39:22 +0700

    Login and Verification

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:deliveryapplication_mobile_driver/screens/homepage_screen.dart';
+import 'package:deliveryapplication_mobile_driver/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
   void main() {
   runApp(const MaterialApp(
-    home: DriverHomePage(),
+    home: LoginPage(),
   ));
 }
